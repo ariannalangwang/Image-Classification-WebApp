c6eaad66728034aea4ad6e9bbffec3a5970f1de1
Lang Wang <langwang@Langs-MacBook-Pro.local>
2021-02-15 15:14:26 -0800

    make it better

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from app import views
 app = Flask(__name__)
 
 # url
-app.add_url_rule('/base','base', views.base)
-app.add_url_rule('/','index', views.index)
+app.add_url_rule('/base', 'base', views.base)
+app.add_url_rule('/', 'index', views.index)
 app.add_url_rule('/faceapp', 'faceapp', views.faceapp)
 app.add_url_rule('/faceapp/photo', 'photo', views.photo, methods=['GET','POST'])
 #
